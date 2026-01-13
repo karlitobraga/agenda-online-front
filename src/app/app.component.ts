b163a98334abf7eac6f77c75c1a8bce3fd50eef2
Carlito <karlitobraga@gmail.com>
2026-01-13 11:40:47 -0300

    Icone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoaderComponent } from './components/shared/loader/loader.component';
 import { PwaService } from './services/pwa.service';
@@ -14,6 +14,11 @@ import { PwaService } from './services/pwa.service';
 export class AppComponent {
   title = 'tinypet';
   public pwaService = inject(PwaService);
+  private router = inject(Router);
+
+  showInstallButton() {
+    return this.pwaService.showInstallButton() && !this.router.url.startsWith('/agendar');
+  }
 
   constructor() {
     // Just injecting it is enough to start listening
