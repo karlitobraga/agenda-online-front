c9de9e3de3532e0ca4d66b3b6ed817a252d9a8d4
Carlito <karlitobraga@gmail.com>
2026-01-13 11:16:39 -0300

    c bug

--- src/app/services/pwa.service.ts
+++ src/app/services/pwa.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
@@ -7,22 +8,24 @@ export class PwaService {
     private deferredPrompt: any;
     showInstallButton = signal(false);
 
-    constructor() {
-        window.addEventListener('beforeinstallprompt', (e) => {
-            // Prevent the mini-infobar from appearing on mobile
-            e.preventDefault();
-            // Stash the event so it can be triggered later.
-            this.deferredPrompt = e;
-            // Update UI notify the user they can install the PWA
-            this.showInstallButton.set(true);
-        });
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+        if (isPlatformBrowser(this.platformId)) {
+            window.addEventListener('beforeinstallprompt', (e) => {
+                // Prevent the mini-infobar from appearing on mobile
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                this.deferredPrompt = e;
+                // Update UI notify the user they can install the PWA
+                this.showInstallButton.set(true);
+            });
 
-        window.addEventListener('appinstalled', (evt) => {
-            // Log install to analytics
-            console.log('INSTALL: Success');
-            this.deferredPrompt = null;
-            this.showInstallButton.set(false);
-        });
+            window.addEventListener('appinstalled', (evt) => {
+                // Log install to analytics
+                console.log('INSTALL: Success');
+                this.deferredPrompt = null;
+                this.showInstallButton.set(false);
+            });
+        }
     }
 
     installPwa() {
